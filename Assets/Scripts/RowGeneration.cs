5c892f93b001b8a212185bca54366a673a83f369
Dorian <dorianmuhametaj0920@gmail.com>
2025-09-07 16:58:36 -0400

    Made it so that when you click the button debug.drawline draws the number of squares that is inputed in the SquareNumberInput field

--- Assets/Scripts/RowGeneration.cs
+++ Assets/Scripts/RowGeneration.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class RowGeneration : MonoBehaviour
 {
     public int SquareNumberInput;
+    Vector2 start = Vector2.one;
+    Vector2 end = Vector2.one;
+    Vector2 startParallel;
+    Vector2 endParallel;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +19,30 @@ public class RowGeneration : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Go()
+    {
+        start.x *= -11;
+        end.x *= (-11 + SquareNumberInput);
+
+        startParallel = start;
+        startParallel.y -= 1;
+
+        endParallel = end;
+        endParallel.y -= 1; 
+        
+        Debug.DrawLine(start, end, Color.red, 100f);
+        Debug.DrawLine(startParallel, endParallel, Color.red, 100f);
+
+        for(int i = 0; i < SquareNumberInput + 1; i++)
+        {
+            start.x += i;
+            startParallel.x += i; 
+            Debug.DrawLine(start, startParallel, Color.red, 100f);
+            start.x -= i;
+            startParallel.x -= i; 
+        }
+
     }
 }
