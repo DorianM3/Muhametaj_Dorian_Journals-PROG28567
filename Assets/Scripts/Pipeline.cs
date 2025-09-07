92077724dd49721eb67a6609eb2dc4aac3ddcec4
Dorian <dorianmuhametaj0920@gmail.com>
2025-09-07 18:00:52 -0400

    Made it so every 0.1 seconds the player is holding down the button, we draw a line using Debug.Drawline to draw from their point 0.1 second ago to their current point

--- Assets/Scripts/Pipeline.cs
+++ Assets/Scripts/Pipeline.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Pipeline : MonoBehaviour
 {
-    Vector2 store = new Vector2(0, 0);
+    public float timer;
+    Vector2 store = new Vector2(0, 0); 
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +15,21 @@ public class Pipeline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += 1f * Time.deltaTime; 
         if (Input.GetMouseButton(0))
         {
-            Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(mouse); 
+            if (store.x == 0 && store.y == 0)
+            {
+                store = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            if (timer > 0.1f)
+            {
+                Vector2 playermouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Debug.DrawLine(store, playermouse, Color.red, 100f);
+                timer = 0;
+                store *= Vector2.zero; 
+            }
         }
     }
 }
