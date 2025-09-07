293567c0fe3a4ff7521bc0fd352862e89f676a86
Dorian <dorianmuhametaj0920@gmail.com>
2025-09-07 18:30:06 -0400

    Made it so the console outputs the magnitude of your line upon button release

--- Assets/Scripts/Pipeline.cs
+++ Assets/Scripts/Pipeline.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Pipeline : MonoBehaviour
 {
     public float timer;
-    Vector2 store = new Vector2(0, 0); 
+    Vector2 store = new Vector2(0, 0);
+    Vector2 storevalueStart = new Vector2 (0,0);
+    Vector2 storevalueEnd;
+    Vector2 storevalues; 
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +18,17 @@ public class Pipeline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer += 1f * Time.deltaTime; 
+        timer += 0.5f * Time.deltaTime; 
         if (Input.GetMouseButton(0))
         {
             if (store.x == 0 && store.y == 0)
             {
                 store = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                if (storevalueStart == Vector2.zero)
+                {
+                    storevalueStart = store;
+                }
             }
 
             if (timer > 0.1f)
@@ -30,6 +38,16 @@ public class Pipeline : MonoBehaviour
                 timer = 0;
                 store *= Vector2.zero; 
             }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            storevalueEnd = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            storevalues.x += (storevalueEnd.x - storevalueStart.x);
+            storevalues.y += (storevalueEnd.y - storevalueStart.y);
+            Debug.Log(storevalues.x);
+            Debug.Log(storevalues.y); 
+            Debug.Log(Mathf.Sqrt(Mathf.Pow(storevalues.x, 2) + Mathf.Pow(storevalues.y, 2))); 
         }
     }
 }
