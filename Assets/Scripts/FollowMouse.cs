8cec892413d0ea2aed55bd9bc3fed4c2a6bcf099
Dorian <dorianmuhametaj0920@gmail.com>
2025-09-07 15:51:29 -0400

    Made it so you can scale the square and that the scale of it will effect the debug.drawline positions to match

--- Assets/Scripts/FollowMouse.cs
+++ Assets/Scripts/FollowMouse.cs
@@ -13,8 +13,15 @@ public class FollowMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Makes a vector that tracks mouse position 
         Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+        //Makes a vector out of the objects scale 
+        Vector2 scale = transform.localScale;
+        //Allows the scale to gradually change based on the players scroll inputs 
+        scale.x += Input.mouseScrollDelta.y * 0.1f;
+        scale.y += Input.mouseScrollDelta.y * 0.1f; 
+        //Updates position and scale to match the new numbers influenced by the player 
+        transform.localScale = scale; 
         transform.position = mouse;
     }
 }
--- Assets/Scripts/SquareSpawner.cs
+++ Assets/Scripts/SquareSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class SquareSpawner : MonoBehaviour
 {
+    Vector2 store = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,22 +17,33 @@ public class SquareSpawner : MonoBehaviour
     {
         //Create a vector2 that checks the mouse position 
         Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //Stores the mouse scrolls so they can be added to the points 
+        store.x += Input.mouseScrollDelta.y * 0.05f;
+        store.y += Input.mouseScrollDelta.y * 0.05f;
         //Creates a point that is to the bottom left of the mouse 
         Vector2 point1 = mouse - Vector2.one;
+        point1.x -= store.x; 
+        point1.y -= store.y; 
         //Creates a point that is to the top left of the mouse 
         Vector2 point2 = mouse;
         point2.x -= 1;
         point2.y += 1;
+        point2.x -= store.x; 
+        point2.y += store.y;
         //Creates a point that is to the top right of the mouse 
         Vector2 point3 = mouse + Vector2.one;
-        //Creates a point to the bottom left of the mouse
+        point3.x += store.x; 
+        point3.y += store.y; 
+        //Creates a point to the bottom right of the mouse
         Vector2 point4 = mouse;
         point4.y -= 1;
-        point4.x += 1; 
-
+        point4.x += 1;
+        point4.x += store.x; 
+        point4.y -= store.y; 
         //If a left click input is completed the square will be drawn, connecting the 4 points 
         if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log(point2); 
             Debug.DrawLine(point1, point2, Color.red, 100f);
             Debug.DrawLine(point2, point3, Color.red, 100f);
             Debug.DrawLine(point3, point4, Color.red, 100f);
